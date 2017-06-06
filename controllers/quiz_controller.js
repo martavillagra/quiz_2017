09c62998072c785bb0066b851dec1b4828fb536e
MartaVillagra <m.villagra@alumnos.upm.es>
2017-06-06 16:53:47 +0200

    Con mi p52 y mi nombre

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -209,72 +209,63 @@ exports.play = function (req, res, next) {
     });
 };
 
-// GET /quizzes/randomplay
-exports.randomPlay = function (req, res, next) {
+//GET /quizzes/randomplay
+exports.randomplay = function (req, res, next) {
 
-        if(!req.session.resolved){
-             req.session.resolved = [-1 ];
-            var score = 0;
-        }
-        //Compruebo que el id random que paso no ha sido ya pasado y resuelto el quiz por el usuario
-        var whereOpt =  {id:{$notIn: req.session.resolved}};
+    var answer = req.query.answer || '';
+    if(!req.session.p52 || !req.session.p52.yaPreguntadas){
+	req.session.p52 = { yaPreguntadas : [-1] }; //creo un nuevo atributo que es un array con las ya preguntadas
+    }
 
-        var score = req.session.resolved.length -1;
-        models.Quiz.count({where: whereOpt})
-            .then(function(c){ // c es el numero de preguntas que me quedan
-                // numero aleatorio entre 0 y el numero de respuestas que me quedan (sin incluir)
-                var randomNumber = Math.floor(Math.random()*c);
+    models.Quiz.count({where : {'id': {$notIn: req.session.p52.yaPreguntadas}}}) //me da el numero de preguntas que no he hecho, las que me quedan por responder
+    .then(function (numNoPreguntadas){
+	var aleatoria = Math.floor(Math.random()*numNoPreguntadas); //num entre 0 y las que me quedan por responder 
+	return models.Quiz.findAll({
+		limit: 1, //para que me devuelva solo una
+		offset: aleatoria, //que me de el numero del array que he calculado aleatoriamente 
+		where:  {'id': {$notIn: req.session.p52.yaPreguntadas}} //para que busque solo entre las que me quedan, no las he hecho
+	});
+    })
+    .then(function (arrayQuiz){ //array con la siguiente pregunta a hacer
+	if(arrayQuiz.length === 0){ //si no hay ninguna es que ya he respondido a todas 
+ 		var score = req.session.p52.yaPreguntadas.length-1;
+		delete req.session.p52.yaPreguntadas;
+		res.render('quizzes/random_nomore', { score: score });
 
-                // la promesa1 devuelve array_quizzes (realmente es 1) a la promesa2
-                return models.Quiz.findAll({where:whereOpt,limit:1, offset:randomNumber}) // array_quizzes
-            })
-            .then(function (array_quizzes) {
-                if (array_quizzes.length ==0){// Si ya se han mostrado todas las preguntas de la DB
-                    delete req.session.resolved;
-                    res.render('quizzes/random_nomore',{
-                        score: score
-                    })
-                }
-
-                else{
-                    res.render('quizzes/random_play',{
-                        quiz: array_quizzes[0],
-                        score: score
-                    })
-                }
-            })
-
+	} else {
+		res.render('quizzes/random_play', { 
+			score : req.session.p52.yaPreguntadas.length -1,
+			quiz: arrayQuiz[0]
+		});
+	}
+    })
+    .catch(function (error) {
+	req.flash('error', 'Error al buscar los Quiz: ' + error.message);
+        next(error);
+    });
 };
 
+//GET /quizzes/randomcheck/:quizId?answer=respuesta
+exports.randomcheck = function (req, res, next) {
 
-// GET  /quizzes/randomcheck/:quizId?answer=respuesta
-exports.randomCheck = function (req, res, next) {
-
-
-   var answer = req.query.answer || "";
-
-   var result = (answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim());
-
-    if(!req.session.resolved){
-        req.session.resolved = [-1 ];
-        var score = 0;
+    var answer = req.query.answer || '';
+    var result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
+    var score = 0;
+    if(result){	
+	req.session.p52.yaPreguntadas.push(req.quiz.id);
+	score= req.session.p52.yaPreguntadas.length-1;
+    } else {
+	score= 0;
+	delete req.session.p52.yaPreguntadas;
     }
-
-    // Añado el id al array de preguntas acertadas
-    if (result){
-       req.session.resolved.push(req.quiz.id);
-       var score = req.session.resolved.length-1;
-    } else{
-        delete req.session.resolved;
-        var score = 0;
-    }
+	
 
     res.render('quizzes/random_result', {
-        quiz: req.quiz,
-        score: score,
-        answer: answer,
-        result: result
-    });
+	score: score,
+	answer: answer,
+	result: result,
+	quiz: req.quiz
+   });
 };
 
 
